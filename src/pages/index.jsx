cefd464c9e3ede0260c349bce671e31212ef81e2
mariaclara-rs <mariaclara.rgs@gmail.com>
2021-10-03 14:29:48 -0300

    A*

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,7 @@
+import { removeComments } from "@babel/types";
 import React, { useState } from "react";
 import Node from '../Classes/Node'
-import 'bootstrap/dist/css/bootstrap.min.css';
+//import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Home() {
     const [matStart, setMatStart] = useState([[]])
@@ -20,7 +21,7 @@ export default function Home() {
     }
 
     function shuffle(rows) {
-        const min = 2, max = 5;
+        const min = 2, max = 8;
         var rand = parseInt(min + Math.random() * (max - min));
         var x, y; //coordenas da posição livre
         var found = false
@@ -92,54 +93,77 @@ export default function Home() {
             }
         return sum
     }
+    function equalsMat(mat1, mat2){
+        var diff = false;
+        for(let i=0; i<mat1.length && !diff; i++)
+            for(let k=0; k<mat1[i].length && !diff; k++)
+                if(mat1[i][k]!=mat2[i][k])
+                    diff=true
+        return !diff
+    }
     function AStar() {
         var matrix = [
             [1, 2, 3],
             [4, 0, 5],
             [6, 7, 8]
         ];
+        var path = []
         var matTemp
-        var xy, nb, root
+        var xy, nb, root, posRoot
+        var arrayLeaf = []
         setMatEnd(copyMat(matrix))
         shuffle(matrix)
-        console.log(matEnd)
-        console.log("--")
         var root_ = new Node(matStart);
 
         root = new Node(matStart);
         root.setFa(sumManhattan(root.getData(), matEnd))
-        xy = posEmpty(root.getData())
-        nb = neighbors(xy[0], xy[1])
-        //criar matrizes possiveis
-        for (let i = 0; i < nb.length; i += 2) {
-            matTemp = new Node(copyMat(root.getData()))
-            matTemp.getData()[xy[0]][xy[1]] = matTemp.getData()[nb[i]][nb[i + 1]]
-            matTemp.getData()[nb[i]][nb[i + 1]] = 0
-            matTemp.setFa(sumManhattan(matTemp.getData(), matEnd))
-            matTemp.setFc(sumManhattan(root.getData(), matTemp.getData()))
-            root.getChild().push(matTemp)
-        }
-        console.log(root.getData())
-        console.log(root.getChild())
-        //selecionar filho de menor custo
-        let k
-        var pos, cost, lowestCost
-        if (root.getChild().length > 0) {
-
-            lowestCost = root.getChild()[0].getFa() + root.getChild()[0].getFc()
-            pos = 0
-            for (k = 1; k < root.getChild().length; k++) {
-                cost = root.getChild()[k].getFa() + root.getChild()[k].getFc()
-                if (cost < lowestCost) {
-                    lowestCost = cost
-                    pos = k
-                }
+        var it=0;
+        while(!equalsMat(root.getData(),matEnd)){
+            it++;
+            path.push(root)
+            xy = posEmpty(root.getData())
+            nb = neighbors(xy[0], xy[1])
+            //definir estados seguintes possiveis
+            arrayLeaf.push(root)
+            for (let i = 0; i < nb.length; i += 2) {
+                matTemp = new Node(copyMat(root.getData()))
+                matTemp.getData()[xy[0]][xy[1]] = matTemp.getData()[nb[i]][nb[i + 1]]
+                matTemp.getData()[nb[i]][nb[i + 1]] = 0
+                matTemp.setFa(sumManhattan(matTemp.getData(), matEnd))
+                matTemp.setFc(sumManhattan(root.getData(), matTemp.getData()))
+                root.getChild().push(matTemp)
+                arrayLeaf.push(matTemp)
             }
-            //filho selecionado
-            root = root.getChild()[pos]
-            console.log("filho selecionado")
-            console.log(root.data)
+            if(nb.length>0){
+                //remover root da lista de folhas
+                posRoot = arrayLeaf.indexOf(root)
+                //remove 1 elemento do índice posRoot
+                arrayLeaf.splice(posRoot, 1);
+            }
+
+            //selecionar filho de menor custo
+            let k
+            var pos, cost, lowestCost
+
+            if(arrayLeaf.length > 0) {
+                lowestCost = arrayLeaf[0].getFa() + arrayLeaf[0].getFc()
+                pos = 0
+                for (k = 1; k < arrayLeaf.length; k++) {
+                    cost = arrayLeaf[k].getFa() + arrayLeaf[k].getFc()
+                    if (cost < lowestCost) {
+                        lowestCost = cost
+                        pos = k
+                    }
+                }
+                //filho selecionado
+                root = arrayLeaf[pos]
+            }
         }
+        path.push(root)
+        for(let i=0; i<path.length; i++)
+            console.log(path[i].data)
+        console.log("it: "+it)
+        console.log("tam de path: "+path.length)
     }
 
     function hillClimbing() {
