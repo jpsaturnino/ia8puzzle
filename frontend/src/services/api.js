86a1f6c8cefe0383041187ea13e9a98fb9fe202d
jpsaturnino <jp_saturnino@hotmail.com>
2021-11-05 22:28:47 -0300

    api server updated

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3344'
+  //http://localhost:3344
+  baseURL: 'https://ia8puzzle-server.herokuapp.com'
 });
 
 export default api;
